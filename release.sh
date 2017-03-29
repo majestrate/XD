d20f272a495831bc8624a2a11de4ac7a0ebb2a95
Jeff Becker <ampernand@gmail.com>
2017-03-29 10:28:11 -0400

    add release script

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+version="$1"
+git clean -xdf
+GOOS=windows GOARCH=386 make clean build  && mv XD.exe XD-$version-win32.exe && gpg --sign --detach XD-$version-win32.exe
+GOOS=darwin GOARCH=amd64 make clean build && mv XD XD-$version-darwin && gpg --sign --detach XD-$version-darwin
+GOOS=linux GOARCH=386 make clean build && mv XD XD-$version-linux-i386 && gpg --sign --detach XD-$version-linux-i386
+GOOS=linux GOARCH=amd64 make clean build && mv XD XD-$version-linux-amd64 && gpg --sign --detach XD-$version-linux-amd64
+GOOS=linux GOARCH=arm make clean build && mv XD XD-$version-linux-arm && gpg --sign --detach XD-$version-linux-arm
