3f22ef1c1005c501f11e1af2cf01a595d8d337dd
Jeff Becker <ampernand@gmail.com>
2017-08-13 11:24:54 -0400

    handle nonzero and negative

--- src/xd/lib/rpc/rpc_setopt.go
+++ src/xd/lib/rpc/rpc_setopt.go
@@ -10,11 +10,15 @@ type SetPieceWindowRequest struct {
 }
 
 func (r *SetPieceWindowRequest) ProcessRequest(sw *swarm.Swarm, w *ResponseWriter) {
-	sw.Torrents.MaxReq = r.N
-	sw.Torrents.ForEachTorrent(func(t *swarm.Torrent) {
-		t.MaxRequests = r.N
-	})
-	w.Return(map[string]interface{}{"error": nil})
+	if r.N > 0 {
+		sw.Torrents.MaxReq = r.N
+		sw.Torrents.ForEachTorrent(func(t *swarm.Torrent) {
+			t.MaxRequests = r.N
+		})
+		w.Return(map[string]interface{}{"error": nil})
+	} else {
+		w.SendError("N must be greater than zero")
+	}
 }
 
 func (r *SetPieceWindowRequest) MarshalJSON() (data []byte, err error) {
