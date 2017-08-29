437a90976719a12c2b2420c7ea44f28cdc417052
Jeff Becker <jeff@i2p.rocks>
2017-08-29 09:29:20 -0400

    more

--- contrib/webui/xd.js
+++ contrib/webui/xd.js
@@ -6,3 +6,34 @@ function XDUI()
 {
     this._url = "http://127.0.0.1:1488/ecksdee/api";
 }
+
+
+XDUI.prototype._apicall = function(call, cb)
+{
+    var self = this;
+    $.ajax({
+        url: self._url,
+        contentType: "text/json; charset=UTF-8",
+        content: JSON.stringify(call),
+        success: function(any, text, xhr) {
+            var j = JSON.parse(text);
+            console.log(call, j);
+            cb(j);
+        }
+    });
+};
+
+/** fetch a list of torrents and call a callback on each fetched */
+XDUI.prototype.eachTorrent = function(cb)
+{
+    var self = this;
+    self._apicall({
+
+    }, function(j) {
+        if(j.error) {
+
+        } else {
+
+        }
+    });
+};
