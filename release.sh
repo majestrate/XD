c865d2a8aef123ad9fad7624c926d07aeb92b67c
Jeff Becker <jeff@i2p.rocks>
2017-09-08 17:50:30 -0400

    add ppc release targets

--- release.sh
+++ release.sh
@@ -7,4 +7,6 @@ GOOS=darwin GOARCH=amd64 make clean build && mv XD XD-$version-darwin && gpg --s
 GOOS=linux GOARCH=386 make clean build && mv XD XD-$version-linux-i386 && gpg --sign --detach XD-$version-linux-i386
 GOOS=linux GOARCH=amd64 make clean build && mv XD XD-$version-linux-amd64 && gpg --sign --detach XD-$version-linux-amd64
 GOOS=linux GOARCH=arm make clean build && mv XD XD-$version-linux-arm && gpg --sign --detach XD-$version-linux-arm
+GOOS=linux GOARCH=ppc make clean build && mv XD XD-$version-linux-ppc && gpg --sign --detach XD-$version-linux-ppc
+GOOS=linux GOARCH=ppc64 make clean build && mv XD XD-$version-linux-ppc64 && gpg --sign --detach XD-$version-linux-ppc64
 GOOS=freebsd GOARCH=amd64 make clean build  && mv XD XD-$version-freebsd-amd64 && gpg --sign --detach XD-$version-freebsd-amd64
