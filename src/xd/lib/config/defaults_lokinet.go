d79ff87780d95e6ce1684d5fedd55f9f4ac4e40b
Jeff Becker <jeff@i2p.rocks>
2019-08-11 13:30:08 -0400

    update default tracker list

--- src/xd/lib/config/defaults_lokinet.go
+++ src/xd/lib/config/defaults_lokinet.go
@@ -6,5 +6,5 @@ const DisableLokinetByDefault = false
 const DisableI2PByDefault = true
 
 var DefaultOpenTrackers = map[string]string{
-	"7oki": "http://7okic5x5do3uh3usttnqz9ek3uuoemdrwzto1hciwim9f947or6y.loki:6680/announce",
+	"ICX": "http://icxqqcpd3sfkjbqifn53h7rmusqa1fyxwqyfrrcgkd37xcikwa7y.loki:6680/announce",
 }
