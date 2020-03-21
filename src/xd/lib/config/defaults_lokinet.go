4bc27b39e989d02f761093d1611860464783f48d
Jeff Becker <jeff@i2p.rocks>
2020-03-21 19:27:33 -0400

    update default lokinet tracker

--- src/xd/lib/config/defaults_lokinet.go
+++ src/xd/lib/config/defaults_lokinet.go
@@ -6,5 +6,5 @@ const DisableLokinetByDefault = false
 const DisableI2PByDefault = true
 
 var DefaultOpenTrackers = map[string]string{
-	"ICX": "http://icxqqcpd3sfkjbqifn53h7rmusqa1fyxwqyfrrcgkd37xcikwa7y.loki:6680/announce",
+	"default": "http://azxoy94ffnnqa54pqxhmoc6geaigdrqqkwxzk3jiafazo8x8a73o.loki:6680/annonce",
 }
