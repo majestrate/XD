07c433df87d1991a3c6bf7bdcf0288e0a20c4aed
Jeff Becker <jeff@i2p.rocks>
2020-03-21 19:38:21 -0400

    typo

--- src/xd/lib/config/defaults_lokinet.go
+++ src/xd/lib/config/defaults_lokinet.go
@@ -6,5 +6,5 @@ const DisableLokinetByDefault = false
 const DisableI2PByDefault = true
 
 var DefaultOpenTrackers = map[string]string{
-	"default": "http://azxoy94ffnnqa54pqxhmoc6geaigdrqqkwxzk3jiafazo8x8a73o.loki:6680/annonce",
+	"default": "http://azxoy94ffnnqa54pqxhmoc6geaigdrqqkwxzk3jiafazo8x8a73o.loki:6680/announce",
 }
