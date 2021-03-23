0c1ef7615046a47387a4109843c255e7482ac76d
Jeff Becker <jeff@i2p.rocks>
2021-03-23 08:43:12 -0400

    update lokinet tracker url

--- lib/config/defaults_lokinet.go
+++ lib/config/defaults_lokinet.go
@@ -6,5 +6,5 @@ const DisableLokinetByDefault = false
 const DisableI2PByDefault = true
 
 var DefaultOpenTrackers = map[string]string{
-	"default": "http://azxoy94ffnnqa54pqxhmoc6geaigdrqqkwxzk3jiafazo8x8a73o.loki:6680/announce",
+	"default": "http://probably.loki:6680/announce",
 }
