majestrate/XD

commit 2bdc043e090a7f4b148b4b8cd72fcf9caf9ac577

use go:embed

Makefile
@@ -48,18 +48,11 @@ endif
 
 build: $(CLI)
 
-assets: $(ASSETS)
 
-$(GO_ASSETS):
-	$(GO) build -o $(GO_ASSETS) -v github.com/jessevdk/go-assets-builder
-
-$(ASSETS): $(GO_ASSETS) $(WEBUI_CORE)
-	$(GO_ASSETS) -p assets -s $(WEBUI_PREFIX) -o $(ASSETS) $(WEB_FILES)
-
-$(XD): $(ASSETS)
+$(XD): webui
 	$(GO) build -a -ldflags "-X xd/lib/version.Git=$(GIT_VERSION)" -tags='$(TAGS)' -o $(XD)
 
-dev: $(ASSETS)
+dev: webui
 	$(GO) build -race -v -a -ldflags "-X xd/lib/version.Git=$(GIT_VERSION)" -tags='$(TAGS)' -o $(XD)
 
 $(CLI): $(XD)
@@ -92,6 +85,7 @@ $(WEBUI_CORE): $(WEBUI_LOGO)
 	$(MAKE) -C $(WEBUI)
 
 webui: $(WEBUI_CORE)
+	$(CP) $(WEB_FILES) $(REPO)/lib/rpc/assets/
 
 no-webui:
 	$(GO) build -ldflags "-X xd/lib/version.Git=$(GIT_VERSION)" -o $(XD)

go.mod
@@ -1,6 +1,6 @@
 module github.com/majestrate/XD
 
-go 1.14
+go 1.16
 
 require (
 	github.com/jessevdk/go-assets v0.0.0-20160921144138-4f4301a06e15

lib/rpc/assets/web.go
@@ -4,8 +4,16 @@ package assets
 
 import (
 	"net/http"
+	"embed"
 )
 
+// content holds our static web server content.
+//go:embed favicon.png
+//go:embed xd.min.js
+//go:embed xd.css
+//go:embed index.html
+var content embed.FS
+
 func GetAssets() http.FileSystem {
-	return Assets
+	return http.FS(content)
 }

commit 8cb60fbea5070f4ecc4bcf5d6a634b1fc38bd279

Makefile: fix regression in webui target
The issue in the webui target that was fixed in commit 6b4204e162cef15afcf3e53634c132c78a50f3e5
was reintroduced as a regression by commit 2bdc043e090a7f4b148b4b8cd72fcf9caf9ac577.

Refer to commit 2bdc043e090a7f4b148b4b8cd72fcf9caf9ac577 for a description of the issue.

Signed-off-by: Daniel Bermond <danielbermond@gmail.com>

Makefile
@@ -46,10 +46,10 @@ CLI ?= XD-CLI$(BINEXT)
 build: $(CLI)
 
 
-$(XD): webui
+$(XD): $(WEBUI_CORE)
 	$(GO) build -a -ldflags "-X xd/lib/version.Git=$(GIT_VERSION)" -tags='$(TAGS)' -o $(XD)
 
-dev: webui
+dev: $(WEBUI_CORE)
 	$(GO) build -race -v -a -ldflags "-X xd/lib/version.Git=$(GIT_VERSION)" -tags='$(TAGS)' -o $(XD)
 
 $(CLI): $(XD)
@@ -80,9 +80,9 @@ $(WEBUI_LOGO):
 
 $(WEBUI_CORE): $(WEBUI_LOGO)
 	$(MAKE) -C $(WEBUI)
+	$(CP) $(WEB_FILES) $(REPO)/lib/rpc/assets/
 
 webui: $(WEBUI_CORE)
-	$(CP) $(WEB_FILES) $(REPO)/lib/rpc/assets/
 
 no-webui:
 	$(GO) build -ldflags "-X xd/lib/version.Git=$(GIT_VERSION)" -o $(XD)